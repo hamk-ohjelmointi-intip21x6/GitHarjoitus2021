be6ecb839f2f37d217084f4173e93ddf78b77527
Turo Nylund <turo.nylund@outlook.com>
2021-09-10 09:39:55 +0300

    Lisäsin laskin luokan ja Erotus metodin.

---
+++ GitHarjoitus2021/Laskin.cs
@@ -0,0 +1,11 @@
+﻿namespace GitHarjoitus2021
+{
+    public class Laskin
+    {
+
+        public int Erotus(int luku1, int luku2) {
+            return luku1 - luku2;
+        }
+
+    }
+}
--- GitHarjoitus2021/Program.cs
+++ GitHarjoitus2021/Program.cs
@@ -6,7 +6,13 @@ namespace GitHarjoitus2021
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Terve Maailma!");
+            Console.WriteLine("Terve Laskint!");
+
+
+            Laskin laskimeni = new Laskin();
+            Console.WriteLine("Laske lukujen 5 ja 16 erotus");
+            int erotus = laskimeni.Erotus(5, 16);
+            Console.WriteLine("erotus on: " + erotus);
         }
     }
 }
