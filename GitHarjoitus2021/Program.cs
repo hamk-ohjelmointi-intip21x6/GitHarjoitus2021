3245386ecbdbdd90d52b7d533aa0f2a1632886a3
Ohjelmoija 2 <turo.nylund@outlook.com>
2021-09-10 11:09:14 +0300

    Kirjoitettu pääohjelmaan esimerkkejä laskuista

--- GitHarjoitus2021/Program.cs
+++ GitHarjoitus2021/Program.cs
@@ -15,10 +15,17 @@ namespace GitHarjoitus2021
             int erotus = laskimeni.Erotus(148, 56);
             Console.WriteLine("erotus on: " + erotus);
 
-
             Console.WriteLine("Laske lukujen 3005 ja 453 summa");
             int summa = laskimeni.Summa(3005, 453);
             Console.WriteLine("summa on: " + summa);
+
+            Console.WriteLine("Laske lukujen 74 ja 222 tulo");
+            int tulo = laskimeni.Tulo(74, 222);
+            Console.WriteLine("summa on: " + tulo);
+
+            Console.WriteLine("Laske lukujen 48 ja 87 jaon tulos");
+            int tulos = laskimeni.JakoLasku(48, 87);
+            Console.WriteLine("Jakojäännös on: " + tulos);
 
             Console.WriteLine("Laske lukujen 15 ja 4 jakojäännös");
             int jakojaannos = laskimeni.JakoJaannos(15, 4);
