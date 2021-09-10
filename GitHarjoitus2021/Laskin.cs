ca97014c808ef5164b80fa3fbc0a003193407942
Turo Nylund <turo.nylund@outlook.com>
2021-09-10 11:11:35 +0300

    Laskimeen muutoksia jotka unohdin

--- GitHarjoitus2021/Laskin.cs
+++ GitHarjoitus2021/Laskin.cs
@@ -3,28 +3,37 @@
     public class Laskin
     {
 
-        public int Erotus(int luku1, int luku2) {
-            return luku1 - luku2;
-        }
 
         public int Summa(int luku1, int luku2)
         {
             return luku1 + luku2;
         }
-        
+
+
         /// <summary>
         /// Tulo on kertolaskun  tulos.
         /// </summary>
         /// <param name="luku1"></param>
         /// <param name="luku2"></param>
         /// <returns></returns>
-        public int Tulo(int luku1, int luku2) {
+        public int Tulo(int luku1, int luku2)
+        {
             return luku1 * luku2;
         }
 
         public int JakoLasku(int luku1, int luku2)
         {
             return luku1 / luku2;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="luku1"></param>
+        /// <param name="luku2"></param>
+        /// <returns></returns>
+        public int Erotus(int luku1, int luku2) {
+            return luku1 - luku2;
         }
 
         public int JakoJaannos(int luku1, int luku2)
