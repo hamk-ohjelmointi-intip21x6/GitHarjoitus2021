006f41d23d9e4ebe9f83e6334195afac17992df1
it20 <it20@LAB-A104-0>
2021-09-10 09:31:30 +0300

    Add project files.

---
+++ GitHarjoitus2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHarjoitus2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Terve Maailma!");
+        }
+    }
+}
